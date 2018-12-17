ac231b75c3fcc9460abede0400b821655fe4708e
IamDC <diegocup@live.nl>
2018-12-17 11:20:52 +0100

    commit op verzoek van Anouk

--- PHP/inzendingen.php
+++ PHP/inzendingen.php
@@ -35,85 +35,10 @@ require_once('scripts/checkinzending.php'); // Inzending check script
         </div>
     </header>
     <!--header end-->
-    <style>
-
-    </style>
-
     <!--sidebar start-->
-    <aside>
-        <div class="w3-sidebar w3-bar-block w3-animate-left" style="display:none;z-index:5;background-color: #394a59" id="mySidebar">
-            <div id="sidebar" class="nav-collapse ">
-                <!-- sidebar menu start-->
-                <ul class="sidebar-menu">
-                    <li class="">
-                        <a class="" href="index.php"style="border-right: none;">
-                            <i class="icon_house_alt" style="font-size: 30px;"></i>
-                            <span style="font-size: 25px; padding-top: 10px;">Home</span>
-                        </a>
-                    </li>
-                    <li class="sub-menu">
-                        <a href="javascript:;" class="" style="border-bottom: none;border-right: none;">
-                            <span style="font-size: 25px;">Pubquiz</span>
-                            <span class="menu-arrow arrow_carrot-down"></span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="" href="tables.html" style="border-bottom: none;border-right: none;">
-                            <i class="icon_genius"></i>
-                            <span>Quizzen</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="" href="chart-chartjs.html" style="border-bottom: none;border-right: none;">
-                            <i class="icon_question"></i>
-                            <span>Vragen</span>
-
-                        </a>
-
-                    </li>
-
-                    <br>
-
-                    <li>
-                        <a class="" href="buttons.php" style="border-bottom: none;border-right: none;">
-                            <span style="font-size: 25px;">Top 100</span>
-                            <span class="menu-arrow arrow_carrot-down"></span>
-                        </a>
-
-                    </li>
-                    <li>
-                        <a class="" href="inzendingen.php" style="border-bottom: none;border-right: none;">
-                            <i class="icon_pencil-edit"></i>
-                            <span>Inzendingen</span>
-                        </a>
-                    </li>
-
-                    <li class="sub-menu">
-                        <a href="tablestructures.php" class="" style="border-bottom: none;border-right: none;">
-                            <i class="icon_table"></i>
-                            <span>Tables</span>
-                        </a>
-                    </li>
-
-                    <li class="sub-menu ">
-                        <a href="javascript:;" class="" style="border-bottom: none;border-right: none;">
-                            <i class="icon_documents_alt"></i>
-                            <span>Pages</span>
-                        </a>
-                        <ul class="sub">
-                            <li><a class="" href="buttons.php">Buttons</a></li>
-                            <li><a class="" href="login.php"><span>Login</span></a></li>
-                            <li><a class="" href="contact.html"><span>Contact Page</span></a></li>
-                            <li><a class="active" href="blank.html">Blank Page</a></li>
-                            <li><a class="" href="404.html">404 Error</a></li>
-                        </ul>
-                    </li>
-
-                </ul>
-                <!-- sidebar menu end-->
-            </div>
-        </div>
-    </aside>
+<?php
+include_once "sidebar.php";
+?>
     <!--sidebar end-->
     <div class="w3-overlay w3-animate-opacity" onclick="w3_close()" style="cursor:pointer" id="myOverlay"></div>
     <!--main content start-->
